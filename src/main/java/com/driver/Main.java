02d455915eacd9f8f02fdb96fdaededa9a06ca2f
NehaSharma112 <neha40111@gmail.com>
2024-12-20 06:38:26 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "Neha";
+        //error: java: name has private access in com.driver.RWOnly
+        obj.setName("Neha");
+        obj.getName();
+    }
   
 }
